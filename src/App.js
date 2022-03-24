bb1bc18a72d03365fbee8245e5b32506a6478632
Antonina Chichikalova <tonia.chichikalova@gmail.com>
2022-03-24 15:28:08 -0400

    install react icons, add layout and style for Skills component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import About from './components/About';
+import Skills from './components/Skills';
 
 function App() {
   return (
     <div className="App">
       <About />
+      <Skills />
     </div>
   );
 }
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React from 'react';
+import './Skills.scss';
+import { GrReactjs, GrNode, GrGithub } from 'react-icons/gr';
+import { SiExpress, SiPostgresql, SiHtml5, SiJavascript } from 'react-icons/si';
+import { DiSass, DiJqueryLogo, DiNpm, DiCss3Full } from 'react-icons/di';
 
 const Skills = () => {
   return (
-    <div>Skills</div>
+    <div className='skills'>
+      <GrNode className='icon' title='Node.js' />
+      <SiExpress className='icon' title='Express.js' />
+      <SiPostgresql className='icon' title='PostgreSQL' />
+      <GrGithub className='icon' title='GitHub' />
+      <DiCss3Full className='icon' title='CSS3' />
+      <SiHtml5 className='icon' title='HTML5' />
+      <SiJavascript className='icon' title='JS' />
+      <GrReactjs className='icon' title='React.js' />
+      <DiSass className='icon' title='Sass' />
+      <DiJqueryLogo className='icon' title='jQuery' />
+      <DiNpm className='icon' title='npm' />
+    </div>
   )
 }
 

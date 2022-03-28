1c9a8674f380f2da8d3dcdd4aec0df04e99a6211
Antonina Chichikalova <tonia.chichikalova@gmail.com>
2022-03-28 17:30:14 -0400

    install react-parallax and add it to the component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import './App.css';
 import About from './components/About';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
+import { ParallaxProvider } from 'react-scroll-parallax';
 
 function App() {
   return (
-    <div className="App">
+    <ParallaxProvider>
       <About />
       <Skills />
       <Projects />
-    </div>
+    </ParallaxProvider>
   );
 }
 
--- src/components/Project.js
+++ src/components/Project.js
@@ -2,32 +2,40 @@ import React from "react";
 import "./Project.scss";
 import { FiExternalLink } from "react-icons/fi";
 import { AiFillGithub } from "react-icons/ai";
+import { Parallax } from "react-scroll-parallax";
 
 const Project = () => {
   return (
     <div className="project">
-      <div className="content">
-        <h3 className="title">My Project</h3>
-        <img src="https://github.com/achichikalova/Stacks/blob/main/frontend/public/static/images/readme/main_page.png?raw=true" alt="title" />
-        <p className="description">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas,
-          architecto. Accusantium obcaecati ipsa voluptates velit.
-        </p>
-        <ul className="tech">
-          <li>React.js</li>
-          <li>Node.js</li>
-          <li>Express.js</li>
-          <li>Postgres</li>
-        </ul>
-        <div className="links">
-          <a href="#">
-            <FiExternalLink />
-          </a>
-          <a href="#">
-            <AiFillGithub />
-          </a>
+      <Parallax speed={10}>
+        <div className="content">
+          <h3 className="title">My Project</h3>
+          <div className="description">
+            <img
+              src="https://github.com/achichikalova/Stacks/blob/main/frontend/public/static/images/readme/main_page.png?raw=true"
+              alt="title"
+            />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas,
+              architecto. Accusantium obcaecati ipsa voluptates velit.
+            </p>
+          </div>
+          <ul className="tech">
+            <li>React.js</li>
+            <li>Node.js</li>
+            <li>Express.js</li>
+            <li>Postgres</li>
+          </ul>
+          <div className="links">
+            <a href="#">
+              <FiExternalLink />
+            </a>
+            <a href="#">
+              <AiFillGithub />
+            </a>
+          </div>
         </div>
-      </div>
+      </Parallax>
     </div>
   );
 };

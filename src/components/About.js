d0df04a66f1fbdf3a0411eeefdd242417934bc3c
Antonina Chichikalova <tonia.chichikalova@gmail.com>
2022-03-29 21:26:56 -0400

    implement layout of the component

---
+++ src/components/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './About.scss';
+import Animation from './animation/Animation';
+
+const About = () => {
+  return (
+    <section className='about'>
+      {/* <div className="description">
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum eius reiciendis expedita quaerat possimus quibusdam consequuntur ut vitae fugiat, aut ipsa corrupti, doloribus dolorum tenetur.</p>
+      </div> */}
+      <Animation />
+    </section>
+  )
+}
+
+export default About;

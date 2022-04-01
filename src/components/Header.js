451243f9c57c93665bc1821f3f88c72400682875
Antonina Chichikalova <tonia.chichikalova@gmail.com>
2022-04-01 10:06:51 -0400

    WIP, add nav and logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import Loader from "./animation/Loader";
+import "./Header.scss";
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
 
-export default Header
+  return (
+    <div className="header-container">
+      <Loader className='logo' />
+      <nav className="nav">
+        <a href="#intro">Intro</a>
+        <a href="#projects">Projects</a>
+        <a href="#about">About</a>
+        <button className="resume"><span>resume</span></button>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;

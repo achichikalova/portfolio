d45a6a689b9c95d9026894b4711d6585686f33a2
Antonina Chichikalova <tonia.chichikalova@gmail.com>
2022-04-01 14:02:35 -0400

    add header collapse on scroll effect to the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,15 +3,37 @@ import Loader from "./animation/Loader";
 import "./Header.scss";
 
 const Header = () => {
+  const [scrollUp, setScrollUp] = useState(false);
+  const [y, setY] = useState(0);
+  const [top, setTop] = useState(true);
+
+  const handleScroll = (e) => {
+    const window = e.currentTarget;
+    if (y > window.scrollY) {
+      setScrollUp(true);
+    } else if (y < window.scrollY) {
+      setScrollUp(false);
+    } else if (y > 0) {
+      setTop(false);
+    }
+    setY(window.scrollY);  
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', e => handleScroll(e));
+    return () => {
+      window.removeEventListener('scroll', (e) => handleScroll(e));
+    }
+  }, [y]);
 
   return (
-    <div className="header-container">
-      <Loader className='logo' />
+    <div className="header-container" style={{opacity: scrollUp ? '1' : '0', backgroundColor: scrollUp && !top ? '#3636367e' : 'transparent'}}>
+      <Loader />
       <nav className="nav">
         <a href="#intro">Intro</a>
         <a href="#projects">Projects</a>
         <a href="#about">About</a>
-        <button className="resume"><span>resume</span></button>
+      <button className="resume">resume</button>
       </nav>
     </div>
   );

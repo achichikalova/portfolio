846fd3d9f357a728cd6f5b752e50c8e886c73d84
Antonina Chichikalova <tonia.chichikalova@gmail.com>
2022-04-01 14:58:46 -0400

    fix scroll collapse bug

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,6 +6,8 @@ const Header = () => {
   const [scrollUp, setScrollUp] = useState(false);
   const [y, setY] = useState(0);
   const [top, setTop] = useState(true);
+  console.log(y);
+  console.log(top);
 
   const handleScroll = (e) => {
     const window = e.currentTarget;
@@ -13,27 +15,42 @@ const Header = () => {
       setScrollUp(true);
     } else if (y < window.scrollY) {
       setScrollUp(false);
-    } else if (y > 0) {
-      setTop(false);
     }
-    setY(window.scrollY);  
+    setY(window.scrollY);
   };
 
-  useEffect(() => {
-    window.addEventListener('scroll', e => handleScroll(e));
-    return () => {
-      window.removeEventListener('scroll', (e) => handleScroll(e));
+  const handleTop = () => { 
+    if (y === 0) {
+      setTop(true);
+    } else {
+      setTop(false);
     }
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", (e) => handleScroll(e));
+    handleTop();
+    return () => {
+      window.removeEventListener("scroll", (e) => handleScroll(e));
+    };
   }, [y]);
 
   return (
-    <div className="header-container" style={{opacity: scrollUp ? '1' : '0', backgroundColor: scrollUp && !top ? '#3636367e' : 'transparent'}}>
+    <div
+      className="header-container"
+      style={{
+        opacity: scrollUp || top ? "1" : "0",
+        backgroundColor: scrollUp && !top ? "#3636367e" : "transparent",
+      }}
+    >
       <Loader />
       <nav className="nav">
         <a href="#intro">Intro</a>
         <a href="#projects">Projects</a>
         <a href="#about">About</a>
-      <button className="resume">resume</button>
+        <a className="resume">
+          <span>resume</span>
+        </a>
       </nav>
     </div>
   );

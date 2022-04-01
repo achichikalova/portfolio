18c82175da46ec7c9da906df70c77c311f1288a7
Antonina Chichikalova <tonia.chichikalova@gmail.com>
2022-04-01 10:08:23 -0400

    add header component

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import Skills from "./components/Skills";
 import { ParallaxProvider } from "react-scroll-parallax";
 import About from "./components/About";
 import Quote from "./components/Quote";
+import Header from "./components/Header";
 
 function App() {
   return (
     <ParallaxProvider>
+      <Header />
       <Intro />
       <Skills />
       <Projects />

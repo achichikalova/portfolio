3a093bc39b1069885d45d9169fc4fdb334b396da
Antonina Chichikalova <tonia.chichikalova@gmail.com>
2022-04-04 17:18:42 -0400

    add media queries for styles

--- src/data.js
+++ src/data.js
@@ -3,17 +3,9 @@ export const projectsData = [
     title: 'Stacks',
     img: 'https://github.com/achichikalova/Stacks/blob/main/frontend/public/static/images/readme/main_page.png?raw=true',
     description: 'A budgeting app designed to help one track their expenses and user patterns/trends to help maintain finances.',
-    stacks: ['React.js', 'Node.js/Express', 'PSQL', 'Material-UI', 'CSS', 'SASS'],
+    stacks: ['React.js', 'Node.js/Express', 'PSQL', 'Material-UI', 'SASS'],
     source: 'https://github.com/achichikalova/Stacks',
     demo: ''
-  },
-  {
-    title: 'To Do App',
-    img: 'https://github.com/achichikalova/to-do-list-react/blob/main/client/public/main.png?raw=true',
-    description: 'The CRUD app allows users to add new tasks into the To-Do list, delete it, mark them as done.',
-    stacks: ['React.js', 'Node.js/Express', 'PSQL', 'Material-UI'],
-    source: 'https://github.com/achichikalova/to-do-list-react',
-    demo: 'https://todoapp-7969c.web.app/'
   },
   {
     title: 'Netflix Clone',
@@ -22,5 +14,13 @@ export const projectsData = [
     stacks: ['React.js', 'JavaScript', 'CSS', 'MovieDB API'],
     source: 'https://github.com/achichikalova/netflix_clone',
     demo: 'https://netflix-clone-4e8a2.web.app/'
+  },
+  {
+    title: 'To Do App',
+    img: 'https://github.com/achichikalova/to-do-list-react/blob/main/client/public/main.png?raw=true',
+    description: 'The CRUD app allows users to add new tasks into the To-Do list, delete it, mark them as done.',
+    stacks: ['React.js', 'Node.js/Express', 'PSQL', 'Material-UI'],
+    source: 'https://github.com/achichikalova/to-do-list-react',
+    demo: 'https://todoapp-7969c.web.app/'
   }
 ]

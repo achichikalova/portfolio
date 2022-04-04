23a8704114d8fd60f2ad644df6ad165851c29fd5
Antonina Chichikalova <tonia.chichikalova@gmail.com>
2022-04-04 19:07:19 -0400

    fix warning errors

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,33 +1,29 @@
 import React, { useEffect, useState } from "react";
-import Loader from "./animation/Loader";
+import Logo from "./animation/Logo";
 import "./Header.scss";
 
 const Header = () => {
   const [scrollUp, setScrollUp] = useState(false);
   const [y, setY] = useState(0);
   const [top, setTop] = useState(true);
-  console.log(y);
-  console.log(top);
-
-  const handleScroll = (e) => {
-    const window = e.currentTarget;
-    if (y > window.scrollY) {
-      setScrollUp(true);
-    } else if (y < window.scrollY) {
-      setScrollUp(false);
-    }
-    setY(window.scrollY);
-  };
-
-  const handleTop = () => { 
-    if (y === 0) {
-      setTop(true);
-    } else {
-      setTop(false);
-    }
-  }
 
   useEffect(() => {
+    const handleScroll = (e) => {
+      const window = e.currentTarget;
+      if (y > window.scrollY) {
+        setScrollUp(true);
+      } else if (y < window.scrollY) {
+        setScrollUp(false);
+      }
+      setY(window.scrollY);
+    };
+    const handleTop = () => { 
+      if (y === 0) {
+        setTop(true);
+      } else {
+        setTop(false);
+      }
+    }
     window.addEventListener("scroll", (e) => handleScroll(e));
     handleTop();
     return () => {
@@ -43,7 +39,7 @@ const Header = () => {
         backgroundColor: scrollUp && !top ? "#3636367e" : "transparent",
       }}
     >
-      <a href="/"><Loader /></a>
+      <a href="/"><Logo /></a>
       <nav className="nav">
         <a href="#intro">Intro</a>
         <a href="#projects">Projects</a>

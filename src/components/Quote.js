3d24995014880ac75453ebdc1d6d58c95ab5ac8d
Antonina Chichikalova <tonia.chichikalova@gmail.com>
2022-04-04 21:09:57 -0400

    add carousel component to remder all qoutes

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,12 +1,28 @@
 import React from "react";
 import "./Quote.scss";
 import { ImQuotesLeft } from "react-icons/im";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
 
 const Quote = () => {
+  const quotes = [
+    "Truth can only be found in one place: the code.",
+    "Clean code always looks like it was written by someone who cares.",
+    "Of course bad code can be cleaned up. But it's very expensive.",
+    "Honesty in small things is not a small thing.",
+    "Would you rather Test-First, or Debug-Later ?",
+  ];
+
+  const quote = quotes.map((quote, key) => {
+    return <p key={key}>{quote}</p>;
+  });
+
   return (
     <div className="quote">
-      <ImQuotesLeft className="decor"/>
-      <p>Clean code should read like well-written prose.</p>
+      <ImQuotesLeft className="decor" />
+      <Carousel autoPlay infiniteLoop showArrows={false} interval={10000} showStatus={false} showThumbs={false}>
+        {quote}
+      </Carousel>
       <span>— Robert C. Martin —</span>
     </div>
   );

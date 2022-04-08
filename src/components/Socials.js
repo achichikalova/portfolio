5b327a18726d3be9ba0e23726a68b42d467fc777
Antonina Chichikalova <tonia.chichikalova@gmail.com>
2022-04-08 17:50:54 -0400

    add socials component and style it

---
+++ src/components/Socials.js
@@ -0,0 +1,44 @@
+import React from "react";
+import './Socials.scss';
+import { FiLinkedin, FiGithub, FiMail } from "react-icons/fi";
+
+const Socials = () => {
+  return (
+    <div className="socials">
+      <ul>
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            aria-label="GitHub"
+            href="https://github.com/achichikalova"
+          >
+            <FiGithub className="icon" title="GitHub" />
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            aria-label="LinkedIn"
+            href="https://www.linkedin.com/in/achichikalova"
+          >
+            <FiLinkedin className="icon" title="LinkedIn" />
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            aria-label="Email"
+            href="mailto:tonia.chichikalova@gmail.com"
+          >
+            <FiMail className="icon" title="Email Me" />
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Socials;

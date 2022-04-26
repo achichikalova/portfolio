c56a5a0b50299c40da8833e75ca4577b66b7e17a
Antonina Chichikalova <tonia.chichikalova@gmail.com>
2022-04-26 12:07:06 -0400

    change order of projects

--- src/data.js
+++ src/data.js
@@ -1,5 +1,13 @@
 export const projectsData = [
   {
+    title: "Recipy App",
+    img: "https://raw.githubusercontent.com/achichikalova/recipy/main/public/images/home_page.png",
+    description: "React Application using Spoonacular API. Responsive design.",
+    stacks: ["React.js", "Axios", "SASS"],
+    source: "https://github.com/achichikalova/recipy",
+    demo: "https://recipy-app-react.web.app/",
+  },
+  {
     title: "Stacks",
     img: "https://github.com/achichikalova/Stacks/blob/main/frontend/public/static/images/readme/main_page.png?raw=true",
     description:
@@ -16,13 +24,5 @@ export const projectsData = [
     stacks: ["React.js", "JavaScript", "CSS", "MovieDB API"],
     source: "https://github.com/achichikalova/netflix_clone",
     demo: "https://netflix-clone-4e8a2.web.app/",
-  },
-  {
-    title: "Recipy App",
-    img: "https://raw.githubusercontent.com/achichikalova/recipy/main/public/images/home_page.png",
-    description: "React Application using Spoonacular API. Responsive design.",
-    stacks: ["React.js", "Axios", "SASS"],
-    source: "https://github.com/achichikalova/recipy",
-    demo: "https://recipy-app-react.web.app/",
   },
 ];

d4f6dbe429fd65f108652c0aabeff6fe2fc9a010
Antonina Chichikalova <tonia.chichikalova@gmail.com>
2022-04-26 11:59:57 -0400

    add new project

--- src/data.js
+++ src/data.js
@@ -18,12 +18,11 @@ export const projectsData = [
     demo: "https://netflix-clone-4e8a2.web.app/",
   },
   {
-    title: "To Do App",
-    img: "https://github.com/achichikalova/to-do-list-react/blob/main/client/public/main.png?raw=true",
-    description:
-      "The CRUD app allows users to add new tasks into the To-Do list, delete it, mark them as done.",
-    stacks: ["React.js", "Node.js/Express", "PSQL", "Material-UI"],
-    source: "https://github.com/achichikalova/to-do-list-react",
-    demo: "https://todoapp-7969c.web.app/",
+    title: "Recipy App",
+    img: "https://raw.githubusercontent.com/achichikalova/recipy/main/public/images/home_page.png",
+    description: "React Application using Spoonacular API. Responsive design.",
+    stacks: ["React.js", "Axios", "SASS"],
+    source: "https://github.com/achichikalova/recipy",
+    demo: "https://recipy-app-react.web.app/",
   },
 ];
